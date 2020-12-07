50541f3168c2e233778203a9de9d3adfc03406f2
charlesrosenbauer <charles.rosenbauer@gmail.com>
2020-12-06 23:59:42 -0600

    File loading

--- main.c
+++ main.c
@@ -4,21 +4,40 @@
 #include "stdio.h"
 
 #include "parser.h"
+#include "struct.h"
 
+
+
+
+void loadFile(char* fname, char** buffer, int* fsize){
+  FILE*  pFile = fopen (fname,"r");
+  size_t result;
+  if (pFile == NULL){ printf("Cannot locate exec file."); exit(1); }
+  fseek (pFile , 0 , SEEK_END);
+  *fsize = ftell (pFile);
+  rewind (pFile);
+
+  // allocate memory to contain the whole file:
+  *buffer = malloc (sizeof(uint8_t)*(*fsize));
+  if (buffer == NULL) { printf("Memory error",stderr); exit(2); }
+
+  // copy the file into the buffer:
+  result = fread (*buffer,1,(*fsize),pFile);
+  if (result != (*fsize)) { printf("Reading error",stderr); exit(3); }
+
+  fclose(pFile);
+}
 
 
 int main(){
-	char* text = "\n\
-Hello\n\
-    There\n\
-        Who\n\
-            Are\n\
-                You?";
+	char* text;
+	int   fsize;
+	loadFile("script.pen", &text, &fsize);
 	
 	int  linect;
 	Str* lines = getLines(text, &linect);
 	
 	for(int i = 0; i < linect; i++){
-		printf("LINE %i. SIZE: %i. LEN: %i : \"%s\"\n", i, strlen(lines[i].str), lines[i].len, lines[i].str);
+		printf("LINE %i. SIZE: %lu. LEN: %i. IND: %i. : \"%s\"\n", i, strlen(lines[i].str), lines[i].len, indentation(lines[i]), lines[i].str);
 	}
 }
